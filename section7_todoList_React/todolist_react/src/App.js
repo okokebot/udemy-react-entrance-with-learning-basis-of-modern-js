399a4641f8f2189c032f156deb58ddcef1e6e81f
AtsukiTonomura <tonomura@mfeed.ad.jp>
2021-02-21 13:01:46 +0900

    jsx でベースとなるデザインを記述

--- section7_todoList_React/todolist_react/src/App.js
+++ section7_todoList_React/todolist_react/src/App.js
@@ -1,7 +1,37 @@
+import './style.css';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="ToDO を入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のToDO</p>
+        <ul>
+          <div className="list-row">
+            <li>aaaaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>aaaaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のToDO</p>
+        <ul >
+          <div className="list-row">
+            <li>aaaaaa</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
   );
 }
 
